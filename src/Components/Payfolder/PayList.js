b97e4fbe72704a946a8ab94facc27e5dcb25136a
Tataata13 <emili.edel.ten@gmail.com>
2024-08-22 22:12:09 +0900

    add totalquantity

--- src/Components/Payfolder/PayList.js
+++ src/Components/Payfolder/PayList.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { TiShoppingCart } from "react-icons/ti";
-import { getTotalPrice } from "../../redux/PaySlice";
+import { getTotalPrice, getTotalQuantity } from "../../redux/PaySlice";
 import { useSelector } from "react-redux";
 import { getCartItems } from "../../redux/PaySlice";
 import CartItem from "./CartItem";
@@ -9,12 +9,16 @@ import CartItem from "./CartItem";
 const PayList =()=>{
     const cartItems = useSelector(getCartItems);
     const totalPrice =useSelector(getTotalPrice);
-
+    const totalQuantity = useSelector(getTotalQuantity);
     let [cartOpen, setCartOpen] = useState(false);
     return(
         <div>
-            <TiShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)} className={`shop-cart-button ${cartOpen && 'active'}`} />
-        {cartOpen && (
+            <TiShoppingCart onClick={() => setCartOpen(cartOpen = !cartOpen)}
+            className={`shop-cart-button ${cartOpen && 'active'}`} />
+
+            {totalQuantity > 0 && <span className="item-count">{totalQuantity}</span>}
+
+            {cartOpen && (
             <div className="shop-cart">
             {cartItems.map (cartItem => <CartItem key={cartItem.id} cartItem = {cartItem}/>)} 
             <h3> TOTAL: $ {totalPrice}</h3>
