f1b633e1b7d7909e5e31c2bae67bc553562e07df
Tataata13 <emili.edel.ten@gmail.com>
2024-08-22 22:08:56 +0900

    add classname-btn-quantity

--- src/Components/Payfolder/CartItem.js
+++ src/Components/Payfolder/CartItem.js
@@ -18,7 +18,7 @@ const CartItem =({cartItem})=> {
             <p>Price: $ {foods.price * cartItem.quantity}</p>
             </div>
 
-            <MdDelete onClick={()=> dispatch(removeItemFromCart({cartItemId: cartItem.id})) } />
+            <MdDelete className="btn-quantity" onClick={()=> dispatch(removeItemFromCart({cartItemId: cartItem.id})) } />
         </div>
     )
 }
